ac97abf3e6957abcee8cbd4c6d65aab0ad7928f1
HP <HP@DESKTOP-LALM8QQ>
2022-12-29 19:21:26 +0530

    First Commit

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Student {
+
+	public static void main(String[] args) {
+		ArrayList list = new ArrayList();
+		
+		list.add("Rushikesh");
+		list.add("Bhosale");
+		System.out.println(list);
+
+	}
+
+}
